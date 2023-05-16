fbffd501dac2b0209633215bc65a8511ed200c23
403bluesky <133398920+403bluesky@users.noreply.github.com>
2023-05-16 11:25:22 -0400

    Update streamlit_app.py
    added an entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,3 +79,12 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response)
