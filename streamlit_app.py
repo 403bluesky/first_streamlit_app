7cfd0aeaa6f9da815fd3ffe5014e20853cd28643
403bluesky <133398920+403bluesky@users.noreply.github.com>
2023-05-16 15:00:15 -0400

    Update streamlit_app.py
    another function

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,4 +86,14 @@ if streamlit.button('Add a Fruit to the List'):
 
 
 
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('" + ???? +"')")
+         return "Thanks for adding " + new_fruit
 
+if streamlit.button('Get Fruit List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   my_cnx.close()
+   streamlit.dataframe(my_data_rows)
