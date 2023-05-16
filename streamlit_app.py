67bf5d56b0ecc8926bedd90a616e0b74cdf864fe
403bluesky <133398920+403bluesky@users.noreply.github.com>
2023-05-16 13:40:39 -0400

    Update streamlit_app.py
    fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-create def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
